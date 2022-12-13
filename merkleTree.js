161e63f0b5e150c9301c89d0d8dbcabb233fc846
Payam Beigi <beigi.payam@gmail.com>
2022-12-12 19:59:55 -0800

    Struct Merkle tree with an array of data

--- merkleTree.js
+++ merkleTree.js
@@ -1,9 +1,22 @@
 class MerkleTree {
   constructor(leaves, concat) {
-      this.array = leaves;
-      this.funConcat = concat;
+      this.leaves = leaves;
+      this.concat = concat;
   }
-  getRoot() {
-      return this.funConcat(this.array[0], this.array[1]);
+  getRoot(leaves = this.leaves) { 
+
+    if (leaves.length === 1) {
+      return leaves[0];
+    }
+
+    let layer = [];
+
+    for (let i=0; i < leaves.length; i += 2) {
+        const left = leaves[i];
+        const right = leaves[i + 1];
+        layer.push(this.concat(left, right));
+    }
+
+    return this.getRoot(layer);
   }
 }
