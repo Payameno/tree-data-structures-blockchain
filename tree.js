45d6d01bd9ad96d1d73c2dd42866abbc5a450f93
Payam Beigi <beigi.payam@gmail.com>
2022-12-12 17:02:56 -0800

    Add nodes recursively to the BTS

--- tree.js
+++ tree.js
@@ -7,17 +7,29 @@ class Tree {
 
       if (!this.root) {
           this.root = node;
+          return;
       }
 
-      if (node.data > this.root.data) {
-          this.root.right = node;
-      }
-      
-      if (node.data < this.root.data) {
-          this.root.left = node;
+      let ptr = this.root
+      while (true) {
+        if (node.data > ptr.data) {
+          if (!ptr.right) {
+            ptr.right = node;
+            break;
+          } else {
+            ptr = ptr.right;
+          }
+        }
+
+        if (node.data < ptr.data) {
+          if (!ptr.left) {
+            ptr.left = node;
+            break;
+          } else {
+            ptr = ptr.left;
+          }
+        }
       }
 
   }
 }
-
-module.exports = Tree;
