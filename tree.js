5b99f07682b9ef9a173af53c7f1e8c1e79f7fc99
Payam Beigi <beigi.payam@gmail.com>
2022-12-12 17:46:09 -0800

    Add search number method

--- tree.js
+++ tree.js
@@ -3,33 +3,50 @@ class Tree {
       this.root = null;
   }
 
-  addNode(node) {
-
-      if (!this.root) {
-          this.root = node;
-          return;
-      }
-
-      let ptr = this.root
-      while (true) {
-        if (node.data > ptr.data) {
-          if (!ptr.right) {
-            ptr.right = node;
-            break;
-          } else {
-            ptr = ptr.right;
-          }
-        }
-
-        if (node.data < ptr.data) {
-          if (!ptr.left) {
-            ptr.left = node;
-            break;
-          } else {
-            ptr = ptr.left;
-          }
-        }
-      }
-
+  hasNode(number) {
+    return this.searchRoot(this.root, number)
   }
+
+  searchRoot(root, data) {
+    if (!root) {
+      return false;
+    }
+    if (root.data === data) {
+      return true;
+    }
+    if (root.data > data) {
+      return this.searchRoot(root.left, data);
+    }
+    if (root.data < data) {
+      return this.searchRoot(root.right, data);
+    }
+  }
+
+  addNode(node) {
+    if (!this.root) {
+      this.root = node;
+    }
+    this.addToRoot(this.root, node);
+  }
+
+  addToRoot(root, node) {
+    if (root.data > node.data) {
+      if (!root.left) {
+        root.left = node;
+      }
+      else {
+        this.addToRoot(root.left, node);
+      }
+    }
+
+    if (root.data < node.data) {
+      if (!root.right) {
+        root.right = node;
+      }
+      else {
+        this.addToRoot(root.right, node);
+      }
+    }
+  }
+
 }
