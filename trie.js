511647f328a53fc68a858b96a9d3106738bf8af1
Payam Beigi <beigi.payam@gmail.com>
2022-12-15 00:19:12 -0800

    enable brancing

--- trie.js
+++ trie.js
@@ -8,11 +8,14 @@ class Trie {
     insert(word) {
       let node = this.root;
       for (let i = 0; i < word.length; i++) {
-        node.children[word[i]] = new TrieNode(word[i]);
+        if (!node.children[word[i]]) {
+          node.children[word[i]]= new TrieNode(word[i]);
+        } 
         node = node.children[word[i]];
         if (i == word.length - 1) {
           node.isWord = true;
         }
       }
+
     }
 }
