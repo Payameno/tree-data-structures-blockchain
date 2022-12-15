03d1492b94b02f6866cad7b92ea762cfde17f3a7
Payam Beigi <beigi.payam@gmail.com>
2022-12-14 23:22:12 -0800

    Insert a new word and create a pmt

--- trie.js
+++ trie.js
@@ -4,4 +4,15 @@ class Trie {
     constructor() {
         this.root = new TrieNode(null);
     }
+
+    insert(word) {
+      let node = this.root;
+      for (let i = 0; i < word.length; i++) {
+        node.children[word[i]] = new TrieNode(word[i]);
+        node = node.children[word[i]];
+        if (i == word.length - 1) {
+          node.isWord = true;
+        }
+      }
+    }
 }
